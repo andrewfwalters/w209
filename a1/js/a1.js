24ba159695b04f85cce0b2be11a3b26fe25e0a02
Andrew Walters <andrew@Andrews-MBP.fios-router.home>
2018-06-27 12:05:16 -0400

    formatting

--- a1/js/a1.js
+++ a1/js/a1.js
@@ -224,7 +224,7 @@ var MacroPlotLib = function() {
       handle.attr("cx", macroScale(g));
       goalText.text(function() {
           var macroType = d3.select(this.parentNode).datum();
-          var macroAmount = handle.attr("cx");
+          var macroAmount = handle.attr("cx").toFixed(0);
           return macroAmount + "g of " + macroType;
         });
       //topBox.style("background-color", d3.hsl(g, 0.8, 0.8));
